e8f87a8704c34cbbf615af782cf67a6de2d4c8d4
Ana <fcaioslab03@MacBook-Pro-de-UNAM-24.local>
2020-03-02 15:29:46 -0600

    All systems ready

--- PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/Contents.swift
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/Contents.swift
@@ -17,6 +17,21 @@ func sendValue(message: String){
 }
 //#-end-hidden-code
 
+/*:
+ # Welcome.
+ 
+ Hi and welcome to this easy way to make your own plant.
+ 
+ If you are here, you are one step to the other side, you are going to learn the arts of plantation,
+ from the most basics, to the highest level.
+ 
+ Now, you just need to adapt to this enviroment, so here are some usefull instructions:
+ 
+    1. As you have seen, the left side will help you with instructions, while the right side will show you interactive scenes.
+    2. Sometimes, below this section, you will find some code with previous instructions of what to do. You can edit it as you want.
+    3. Enjoy this travel and investigate on your own more things about to sweeten this knowledge.
+ */
+
 let str = /*#-editable-code str*/"message"/*#-end-editable-code*/
 
 //#-hidden-code
